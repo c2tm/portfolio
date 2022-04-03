75874a52fda52c7fe01e58201bff987123aea65e
c2tm <cyork4839@gmail.com>
2022-04-03 14:16:33 -0400

    made concept for home section, began about section

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='home'>
+        <div>
+            <div className='title-group'>
+              <h1>C2TM</h1>
+              <h2>fullstack developer</h2>
+            </div>
+            <button>LEARN MORE</button>
+          </div>
+      </section>
+
+      <section className='about'>
+
+      </section>
     </div>
   );
 }
